1dd7b5b8c090d337211eb37453295a6a2a93193e
JuanJoCasamitjana <73534780+JuanJoCasamitjana@users.noreply.github.com>
2022-06-18 11:54:00 +0200

    Todo lo necesario para cargar tableros, a excepcion de los tableros

---
+++ MonteCarloTreeSearch/src/main/java/game/utils/Constants.java
@@ -0,0 +1,12 @@
+package game.utils;
+
+public class Constants {
+	public static final String HNEFATAFL = "initialBoards/HNEFATAFL.txt";
+	public static final String TABLUT = "initialBoards/TABLUT.txt";
+	public static final String ARD_RI = "initialBoards/ARD_RI.txt";
+	public static final String BRANDUBH = "initialBoards/BRANDUBH.txt";
+	public static final String TAWLBWRDD = "initialBoards/TAWLBWRDD.txt";
+	public static final String ALEA_EVANGELII = "initialBoards/ALEA_EVANGELII.txt";
+	
+	public static final String ERROR_BOARD_OUT_OF_BOUNDS = "You must index the initial boord with an integer between 1 and 6";
+}
